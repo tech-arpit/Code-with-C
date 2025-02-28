76b68f16195ac098408412e248c6457631b0804d
Arpit Tiwari <arpit.tiwari2403@gmail.com>
2025-02-28 22:30:32 +0530

    Creating an If-Else_Electricity Bill Calculator.c

---
+++ If-Else_Electricity Bill Calculator.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    float bill;
+    scanf("%d",&n);
+    if(n<0){
+        printf("Invalid Input!");
+        return 0;
+    }
+    else if(n<=100){
+        bill = n*5;
+    }
+    else if(n<=300 && n>100){
+         bill = (100 * 5) + ((n - 100) * 7);
+    }
+    else if(n>300){
+         bill = (100 * 5) + (200 * 7) + ((n - 300) * 10);
+    }
+    if(bill<=1200){
+        bill = bill * 90/100;
+    }
+
+    printf("The electricity bill is: %.2f.", bill);
+   
+      
+}
