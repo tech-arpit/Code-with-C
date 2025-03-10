1045753ee1eed69b5ea18425ca55ea672d2077c2
Arpit Tiwari <arpit.tiwari2403@gmail.com>
2025-03-10 19:28:44 +0530

    Solution for Bitwise_Check for Multiple of 4.c
    A program to check if a given integer n is a multiple of 4 using only bitwise operators without  using the modulus operator (%), division operator (/), or loops.

---
+++ Bitwise_Check for Multiple of 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+scanf("%d", &n);
+    if ((n & 3) == 0) {
+        printf("Yes\n"); 
+    } else {
+        printf("No\n");  
+    }
+
+    return 0;
+}
