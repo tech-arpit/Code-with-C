9c6f08b4976d460c678b849f9f8219f0a653494e
Arpit Tiwari <arpit.tiwari2403@gmail.com>
2025-03-15 22:44:22 +0530

    Loops_Finding Divisibility by 3 or 5 but not by both Series.c

---
+++ Loops_Divisible by 3 or 5 but not by both Series.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int n;
+    scanf("%d",&n);
+    for(int i = 1; i<=n; i++){
+        if((i%3==0||i%5==0) && i%15!=0)
+        {
+            printf( "%d ",i);
+        }
+    }    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
