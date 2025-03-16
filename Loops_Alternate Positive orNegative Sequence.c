7adcee0aea1d8efc07fbdd9c89c82688b80626d9
Arpit Tiwari <arpit.tiwari2403@gmail.com>
2025-03-16 21:25:37 +0530

    Loops_Finding Alternate Positive or Negative Sequence.c
    A program to generate the first n terms of a sequence where the numbers alternate between positive and negative integers, starting with 1.

---
+++ Loops_Alternate Positive orNegative Sequence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0) {
+            printf("%d", i); 
+        } else {
+            printf("%d", -i); 
+        }
+        if (i < n) {
+            printf(" ");
+        }
+    }
+    printf("\n"); 
+    return 0;
+}
