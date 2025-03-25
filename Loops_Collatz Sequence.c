4ba4bf904f16bd15602d671994dda1bd7980d4cd
Arpit Tiwari <arpit.tiwari2403@gmail.com>
2025-03-25 15:54:16 +0530

    Loops_Collatz Sequence.c
    A program to generate the Collatz sequence for a given positive integer n. Starting with any positive integer n, Collatz sequence is defined corresponding to n as the numbers formed by the following operations :
    
    If n is even, then n = n / 2.
    If n is odd, then n = 3*n + 1.
    Repeat above steps, until it becomes 1.

---
+++ Loops_Collatz Sequence.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+    if (n < 1 || n > 100000) {
+       return 1; 
+    }
+    printf("%d", n);
+
+ 
+    for ( ;n != 1; ) // starting semicolon tells the end of initilization statement 
+    {
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1; 
+        }
+        printf(" -> %d", n); 
+    }
+    printf("\n"); 
+
+    return 0; 
+}
