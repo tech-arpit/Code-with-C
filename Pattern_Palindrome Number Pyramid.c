9a7118d0307af0fadd9634c33244729a2a3989b9
Arpit Tiwari <arpit.tiwari2403@gmail.com>
2025-04-01 22:23:22 +0530

    Pattern_Creating Palindrome Number Pyramid.c
     A program to print a pyramid pattern with numbers based on the input size n, where n represents the total number of rows of the pattern.
    
    The pattern consists:
    
    Each row starts with spaces for alignment, followed by an increasing sequence of numbers and then a decreasing sequence.
    The rows are symmetrically aligned, forming a pyramid-like pattern.

---
+++ Pattern_Palindrome Number Pyramid.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+int n;
+    scanf("%d",&n);
+
+ 
+   
+    for( int i=1;i<=n;i++){
+      
+      for(int j=1 ; j<=n-i ; ++j)
+      {
+          printf(" ");
+      }
+         int a=i;
+      
+        for(int k=1; k<=(2*i)-1;k++){
+             
+            if(k<=i){
+            printf("%d",k);
+                
+        }
+            
+            else{
+           printf("%d", --a);
+                
+               
+            }
+             
+        }printf("\n");
+
+}}
