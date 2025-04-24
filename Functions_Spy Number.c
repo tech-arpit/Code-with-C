5920de9b9d08efc7f5860f3dd8b94b7f9026a0e2
Arpit Tiwari <arpit.tiwari2403@gmail.com>
2025-04-24 20:44:39 +0530

    Functions_Spy Number.c
    A Spy Number is a number where the sum of its digits equals the product of its digits.

---
+++ Functions_Spy Number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int is_spy_number(int n) {
+    int sum = 0;
+   int product = 1 ;
+    while(n!=0){
+        int digit = n%10;
+        product *= digit;
+        sum += digit;
+         n = n/10;
+        }
+    if (sum == product){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+    }
+
+int main() {
+    int n;
+    scanf("%d", &n); 
+    printf("%d\n", is_spy_number(n));
+   
+       
+    return 0;
+}
